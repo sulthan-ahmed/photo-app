30ce01c3af4c88f03b8b538b1be6763924af8487
Sulthan Ahmed <sultanahmeduk@gmail.com>
2015-12-10 15:06:50 +0000

    determined what is __dirname

--- server.js
+++ server.js
@@ -2,15 +2,19 @@
 var express      = require("express");
 var instagram   = require('instagram-node').instagram();
 
-var path = require('path');
-
 //set up the router and server
 var app = express();
 
 // setup render engine, hogan
 app.set('view engine', 'ejs');
 
+//__dirname is string referring to the name of the directory that this script resides in.
+// e.g. /Users/Sulthan/Development/training/photo-app
+// local to each module so it will change in each module
+app.use(express.static(__dirname + '/public'));
+
 //start server
 app.listen(7070, function(){
     console.log("Server running on http://localhost:7070");
+    console.log("the variable __dirname is" + " " + __dirname);
 });
