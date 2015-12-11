143f75a781fbd9f2c577b8f2cb5ba461ac8aec0d
sulthan-ahmed <sulthanahmeduk@gmail.com>
2015-12-11 14:36:49 +0000

    1. amended the header class 2.  started using the instagram api

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 //imports or libraries
 var express      = require("express");
-var instagram   = require('instagram-node').instagram();
+var instagram = require('instagram-node').instagram();
 
 //set up the router and server
 var app = express();
@@ -13,6 +13,18 @@ app.use(express.static(__dirname + '/public'));
 
 // setup render engine, hogan
 app.set('view engine', 'ejs');
+
+instagram.use({
+    client_id: '2571c3a36a7a4a60809651f64bdfe8d3',
+    client_secret: 'd6971f3f37b446f7b6ccb35b70df0e6e'
+});
+
+app.get('/', function(req, res){
+    instagram.media_popular(function (err, medias, remaining, limit){
+
+        res.render('public/views/main/index', {grams: medias})
+    });
+});
 
 //start server
 app.listen(7070, function(err){
